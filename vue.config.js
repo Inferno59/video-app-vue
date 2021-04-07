7bcf4a4514db323cfa458b9862220226caf82fc3
Zorich Ilya <zorichi@br-automation.com>
2021-04-07 16:24:48 +0500

    Before fetch from remote origin

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,7 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/video-app-vue/'
-    : '/',
+  // publicPath: process.env.NODE_ENV === 'production'
+  //   ? '/'
+  //   : '/',
+  publicPath: './',
+  outputDir: 'dist',
 };
